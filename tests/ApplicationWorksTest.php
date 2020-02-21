15c760e06780f315dbeff215fe110203339a3382
Danny Torres <danny.torresxd@gmail.com>
2020-02-21 07:55:43 -0500

    Adding the unit test base

---
+++ tests/ApplicationWorksTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ApplicationWorksTest extends TestCase
+{
+    /**
+     * This assertion it's only for check that phpunit is working
+     */
+    public function testSimpleAssertion(): void
+    {
+        $this->assertTrue(true);
+    }
+}
